83f15403132e5352b17071b92bcd963fa7def413
David <davce449@student.liu.se>
2022-10-03 15:16:56 +0200

    fuck ur walla

--- generate.py
+++ generate.py
@@ -1,35 +1,44 @@
 import csv
 import matplotlib.pyplot as plt
-with open('./data/m_cck1_close_1.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    x = []
-    y = []
-    for row in spamreader:
-        time=row[6].split("-")[0]
-        bytes=row[7]
-        lostpacket=row[10]
-        packets=row[11]
-        jitter=row[9]
-        loss=row[12]
-        outoforder=row[13]
-        if(loss=="-nan"): break
-        x.append(time)
-        y.append(loss)
-        print(time,bytes,jitter,loss,outoforder)
-    
-    print(x)
-    print(y)
+# with open('./data/m_cck1_close_1.csv', newline='') as csvfile:
+#     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+#     x = []
+#     y = []
+#     for row in spamreader:
+#         time=float(row[6].split("-")[0])
+#         bytes=float(row[7])
+#         lostpacket=float(row[10])
+#         packets=row[11]
+#         jitter=row[9]
+#         loss=float(row[12])
+#         outoforder=row[13]
+#         if(loss=="-nan"): break
+#         x.append(time)
+#         y.append(loss)
+#         print(time,bytes,jitter,loss,outoforder)
 
+def generate_graph(datas):
+    
+    x = range(0, 30)
+    for j in range(0, len(datas)):
+        y= []
+        for i in x:
+            if i > len(datas[j]):
+                break
+            y.append(datas[j][i].loss)
+        plt.plot(x, y, marker='o')
+            
     # plotting the points 
-plt.plot(x, y)
-  
-# naming the x axis
-plt.xlabel('x - axis')
-# naming the y axis
-plt.ylabel('y - axis')
-  
-# giving a title to my graph
-plt.title('My first graph!')
-  
-# function to show the plot
-plt.show()
+    
+
+    # naming the x axis
+    plt.xlabel('x - axis')
+    # naming the y axis
+    plt.ylabel('y - axis')
+    
+    plt.axis([0, 40, 0, 100])
+    # giving a title to my graph
+    plt.title('My first graph!')
+    
+    # function to show the plot
+    plt.show()
---
+++ main.py
@@ -0,0 +1,11 @@
+from reader import *
+from generate import *
+
+walla = read("./data/m_cck1_close_0.5.csv")
+balla = read("./data/m_cck1_close_1.csv")
+dalla = read("./data/m_cck1_close_3.csv")
+x=[]
+x.append(walla)
+x.append(balla)
+x.append
+generate_graph(x)
